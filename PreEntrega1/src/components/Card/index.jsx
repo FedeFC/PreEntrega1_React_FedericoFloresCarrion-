3ed7e506be483436a2f9a9604eb5d10999b35d1c
FedericoFc <f.florescarrion@gmail.com>
2023-03-28 18:28:52 -0300

    estilos

--- PreEntrega1/src/components/Card/index.jsx
+++ PreEntrega1/src/components/Card/index.jsx
@@ -5,14 +5,14 @@ import styles from './card.module.scss'
 const Card = ({producto}) => {
   return (
     <Link to={`/item/${producto.id}`} style={{textDecoration:'none'}} >
-    <div className={styles.cardContainer}>
-        <h3> {producto.title}</h3>
-        <img src={producto.image} alt={producto.title} className={styles.imagenes} width='200' height='200' />
-        <div className={styles.preciosContainer}>
-            <p className={styles.precios}>$ {producto.price} </p>
-        </div>
-
-    </div>
+      <div className={styles.cardContainer}>
+          <h3> {producto.title}</h3>
+          <img src={producto.image} alt={producto.title} className={styles.imagenes} width='200' height='200' />
+          <div className={styles.preciosContainer}>
+              <p className={styles.precios}>$ {producto.price} </p>
+          </div>
+            <button className={styles.detallesBtn} >Más detalles</button>
+      </div>
     </Link>
   )
 }
